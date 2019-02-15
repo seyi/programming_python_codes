86fd28e077a3dd699d984872bed063e86404e6f6
seyi <seyiakadri@gmail.com>
2019-02-15 17:49:51 +0100

    class shelving
    class shelving

---
+++ people-alternative.py
@@ -0,0 +1,45 @@
+"""
+alternative implementation of person classed
+data, behaviour and operator overloading
+"""
+
+class Person:
+    """
+    A general person : data + logic
+    """
+    def __init__(self,name,age,pay=0,job=None) :
+        self.name = name
+        self.age = age
+        self.pay = pay
+        self.job = job
+
+    def lastName(self) :
+        return self.name.split()[-1]
+    def giveRaise(self,percent) :
+        self.pay *= (1.0 + percent)
+    def __str__(self):
+        return '<%s => %s>' % (self.__class__.__name__,self.name)
+
+
+class Manager(Person) :
+    """
+    A person with custom raise
+    inherits general,lastName,str
+    """
+
+    def __init__(self,name,age,pay) :
+        Person.__init__(self,name,age,pay,'manager')
+
+    def giveRaise(self,percent,bonus=0.1):
+         Person.giveRaise(self,percent + bonus)
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith',44)
+    sue = Person('Sue Jones',47,40000,'music')
+    tom = Manager(name='Tome Doe',age=50,pay=50000)
+
+    print sue,sue.pay,sue.lastName()
+
+    for obj in [bob,sue,tom] :
+        obj.giveRaise(.10)
+        print obj, obj.pay
---
+++ person_start.py
@@ -0,0 +1,18 @@
+class  Person:
+    def __init__(self,name,age,pay=0,job=None):
+        self.name = name
+        self.age = age
+        self.pay = pay
+        self.job = job
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith',42,30000,'sweng')
+    sue = Person('Sue Jones',45,40000,'music')
+
+    print bob.name, sue.pay
+
+    print bob.name.split()[-1]
+    sue.pay *= 1.10
+    print sue.pay
+
+    
